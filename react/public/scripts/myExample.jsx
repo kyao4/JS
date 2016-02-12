b25c60ac7de56a09fb168f4b0ea64f6163842d50
kyao4 <kyao4@hawk.iit.edu>
2016-02-12 14:59:48 -0600

    finished react tutorial

---
+++ react/public/scripts/myExample.jsx
@@ -0,0 +1,13 @@
+/**
+ * Created by K.Yao on 2016/2/11.
+ */
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+      Hello, world! I am a CommentBox.
+    </div>
+    )
+  }
+});
