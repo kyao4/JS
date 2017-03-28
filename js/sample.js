34777d04298262ceb83afa924dc5348cfe08fc1f
kyao4 <kyao4@hawk.iit.edu>
2017-03-27 20:41:26 -0500

    get child node with childNodes property and set the text node with nodeValue property

--- js/sample.js
+++ js/sample.js
@@ -2,7 +2,19 @@ function changePic(whichPic) {
     var source = whichPic.getAttribute("href");
     var target = document.getElementById("placeholder");
     target.setAttribute("src", source);
+    var text = whichPic.getAttribute("title");
+    var description = document.getElementById("description");
+    description.firstChild.nodeValue = text;
 }
+
+// function countChildren() {
+// 	var body_elements = document.getElementsByTagName("body")[0].childNodes;
+// 	for (var i = 0; i < body_elements.length;i ++) {
+// 		console.log(body_elements[i].nodeType);
+// 	}
+// }
+
+// window.onload = countChildren;
 // alert(typeof document.getElementById("purchases"));
 // var p = document.getElementsByTagName("p");
 // for (var i = 0; i < p.length; i++) {
