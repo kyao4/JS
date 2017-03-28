b6c23ed7e31e24057103f1c3b8fbb28aa8fbf710
Kyao4 <kyao4@hawk.iit.edu>
2017-03-28 00:14:10 +0000

    add eventhandler and function

--- js/sample.js
+++ js/sample.js
@@ -1,1 +1,15 @@
-alert(typeof document.getElementById("purchases"));
+function changePic(whichPic) {
+    var source = whichPic.getAttribute("href");
+    var target = document.getElementById("placeholder");
+    target.setAttribute("src", source);
+}
+// alert(typeof document.getElementById("purchases"));
+// var p = document.getElementsByTagName("p");
+// for (var i = 0; i < p.length; i++) {
+//     var attri = p[i].getAttribute("title");
+//     if (attri) {
+//         alert(attri);
+//     } else {
+//         alert(attri);
+//     }
+// }
